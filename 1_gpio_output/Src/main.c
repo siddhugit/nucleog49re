199129035464b11ccf545e9a5fe6e5ead8e0d27d
siddharth singh <siddharth.sng@gmail.com>
2025-05-23 11:33:17 -0400

    board headers added

---
+++ 1_gpio_output/Src/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32g491xx.h"
+
+
+#define GPIOAEN (1U<<0)
+#define PIN5 (1U<<5)
+#define LED_USR PIN5
+
+int main()
+{
+	//(1) Enable clock access to GPIOA
+	RCC->AHB2ENR |= GPIOAEN;
+
+	//(2) Set PA5 as output mode
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+
+
+
+	//(3) forever loop
+	for(;;){
+		//(4) toggle output register
+		GPIOA->ODR ^= (LED_USR);
+		//(5) cause delay
+		for(int i = 0;i<1000000;++i){
+
+		}
+	}
+}
